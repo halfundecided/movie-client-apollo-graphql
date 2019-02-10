a515a6176318a4d300b888a563faaf7a34ae495c
Mijeong Ban <mban1@stevens.edu>
2019-02-09 21:44:35 -0500

    styled components added

--- src/queries.js
+++ src/queries.js
@@ -2,12 +2,13 @@ import gql from "graphql-tag";
 
 // query statement can be tested in apollo extension
 export const HOME_PAGE = gql`
-query {
-    movies(limit: 50, rating:7) {
-        id
-        title
-        genres
-        rating
+  {
+    movies(limit: 50, rating: 7) {
+      id
+      title
+      rating
+      genres
+      medium_cover_image
     }
-}
+  }
 `;
